e4d378f7dc5b140b5d04e510eb7922f2dedef44a
Haisong Ding <v-haidin@microsoft.com>
2017-07-24 22:13:33 +0800

    Init

---
+++ include/Sortings.hpp
@@ -0,0 +1,22 @@
+/// all sorting algrorithms: including merge sort...
+
+class Sortings {
+public:
+  // merge sort
+  template <typename Dtype>
+  static void MergeSort(Dtype* input_array, const int N) {
+    return;
+  };
+
+  // insertion sort
+  template <typename Dtype>
+  static void InsertionSort(Dtype* input_array, const int N) {
+    return;
+  }
+
+  // quick sort
+  template <typename Dtype>
+  static void QuickSort(Dtype* input_array, const int N) {
+    return;
+  }
+};
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "Sortings.hpp"
+
+void main() {
+  int a[] = { 1, 3, 4 };
+  Sortings::MergeSort<int>(a, 3);
+}
